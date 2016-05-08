a50639bd3b4e8684b2de30166018fc700c90460e
Luo Ning <947703573@qq.com>
2016-05-08 20:18:35 +0800

    Important!! rebuild the code.
    add Parser &BuildRequest Trait .

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This a test file.
+ * 
+ * @author    Ning Luo <luoning@luoning.me>
+ * @copyright This code is copyright to me.
+ * @license   MIT
+ * @package   lndj/Lcrawl
+ */
 
 require 'vendor/autoload.php';
 
@@ -9,22 +17,45 @@ use GuzzleHttp\Pool;
 use Symfony\Component\DomCrawler\Crawler;
 use Doctrine\Common\Cache\FileCache ;
 
-
+/**
+ * Hhahahahhaahah
+ *  
+ * @example index.php this is a test file.
+ */
 $stu_id = '201201148';
 $password = 'luowei2008';
 
 $user = ['stu_id' => $stu_id, 'stu_pwd' => $password];
 $config = [
-	'ua' => 'Lzjtuxzs Spider v2.0.0',
-	'timeout' => 5.0,
-	'cacheCookie' => false,
-	'cachePrefix' => 'Luonning-'
+    'ua' => 'Lzjtuxzs Spider v2.0.0',
+    'timeout' => 5.0,
+    'cacheCookie' => true,
+    'cachePrefix' => 'Luonning-'
 ];
-
 $client = new Lcrawl('http://xuanke.lzjtu.edu.cn/', $user, $config);
 
-
-$client->login();
+// $client->login();
 // $client->getSchedule();
 // $client->getCet();
-$client->getAll();
+dd($client->getAll());
+
+
+
+/**
+ * Just a debug function
+ * @param Obeject/Array/string $arr                                                             
+ * @param String $hint debug hint
+ * @return void
+ */
+function dd($arr,$hint = '')
+{
+    if (is_object($arr) || is_array($arr)) {
+        echo "<pre>";
+        print_r($arr);
+        echo PHP_EOL . $hint;
+        echo "</pre>";
+    } else {
+        var_dump($arr);
+        echo PHP_EOL . $hint;
+    }
+}
---
+++ src/Parser.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * This a lib to crawl the Academic Network Systems.
+ * You can easely achieve the querying of grade/schedule/cet/free classroom ...
+ * 
+ * @author Ning Luo <luoning@Luoning.me>
+ * @link https://github.com/lndj/Lcrawl
+ * @license  MIT
+ */ 
+
+namespace Lndj;
+
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * This is trait to parser data from HTML.
+ */
+trait  Parser {
+    /**
+     * Paser the schedule data.
+     * @param Object $body 
+     * @return Array
+     */
+    public function parserSchedule($body)
+    {
+        $crawler = new Crawler((string)$body);
+        $crawler = $crawler->filter('#Table1');
+        $schedule = $crawler->children();
+        
+        $format_arr = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+        $data = [];
+        $data_line = [];
+
+        //loop the row
+        for ($i=2; $i <= 10; $i++) { 
+            if ($i % 2 === 0) {
+                //Every 4 lines lack 1 row
+                if ($i % 4 === 0) {
+                    for ($j=1; $j <= 7; $j++) { 
+                        $schedule_info = $schedule->eq($i)->children()->eq($j)->html();
+                        array_push($data_line, $schedule_info);
+                    }   
+                    continue;
+                }
+                //Loop the line
+                for ($j=2; $j <= 8; $j++) { 
+                    $schedule_info = $schedule->eq($i)->children()->eq($j)->html();
+                    array_push($data_line, $schedule_info);
+                }
+            }
+        }
+        //Formate the data array.
+        $data = array_chunk($data_line,5);
+        return array_combine($format_arr, $data);
+    }
+
+    /**
+     * Parser the common table, like grade, cet, chooseClass, etc.
+     * @param type|Object $body 
+     * @param type|string $selector 
+     * @return array
+     */
+    public function parserCommonTable($body, $selector = '#DataGrid1')
+    {
+        $crawler = new Crawler((string)$body);
+        
+        $crawler = $crawler->filter($selector);
+        $cet = $crawler->children();
+        $data = $cet->each(function (Crawler $node, $i) {
+            return $node->children()->each(function (Crawler $node, $j) {
+                return $node->text();
+            });
+        });
+        //Unset the title.
+        unset($data[0]);
+        return $data;
+    }
+}
